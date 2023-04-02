2fda0485093a6b744c76f4b795f3eeccf1a69290
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-04-02 19:41:13 +0300

    Теперь точно все
    Теперь точно все

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import (
     MaxValueValidator,
     MinValueValidator,
-    validate_slug,
 )
 from django.db import models
 from model_utils import Choices
